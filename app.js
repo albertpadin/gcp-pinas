5e165bd0735cebc9cca0237de2eb872d20ad1aac
Albert Padin <albertpadin@gmail.com>
2021-03-27 16:41:25 +0800

    Update app.js

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ app.get('/', (req, res) => {
       </div>
 
       <div class="jumbotron">
-        <h1>Yo!!! Welcome to GCP Pinas!!!! \\o/</h1>
+        <h1>Hello there!!! Welcome to GCP Pinas!!!! \\o/</h1>
       </div>
 
 
